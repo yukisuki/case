3ff7de27ba936807931517e07c18d9124d60d769
yuki <yuki@archlinux>
2025-05-10 10:34:41 +0100

    new cases

--- cases.py
+++ cases.py
@@ -3,7 +3,14 @@ import re
 def no_punctuation(s):
     return re.sub(r'[^\w\s]', ' ', s)
 
+def folder_cases(s):
+    reaplced_and = re.sub(r'[&]', 'and', s)
+    return re.sub(r'[^\w\s\(\)\-\&]', '', reaplced_and)
+
+
 cases = {
     'upper': lambda s: s.upper(),
     'lower': lambda s: s.lower(),
+    'folder': lambda s: folder_cases(s),
+    'pun': lambda s: no_punctuation(s),
 }
